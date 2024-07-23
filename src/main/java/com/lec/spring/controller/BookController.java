4730511c202d3f365877b9cbda894135e8d60d35
yeongjae0113 <dudwo0113@gmail.com>
2024-07-23 16:19:01 +0900

    Commit 2024-07-23

--- src/main/java/com/lec/spring/controller/BookController.java
+++ src/main/java/com/lec/spring/controller/BookController.java
@@ -13,31 +13,37 @@ public class BookController {
 
     private final BookService bookService;
 
+    @CrossOrigin    // cross-origin 요청 허용   (인텔리제이와 vsCode 포트번호가 달라서)
     @GetMapping("/")
     public ResponseEntity<?> home(){
         return new ResponseEntity<>("ok", HttpStatus.OK); // 200
     }
 
+    @CrossOrigin    // cross-origin 요청 허용   (인텔리제이와 vsCode 포트번호가 달라서)
     @PostMapping("/book")
     public ResponseEntity<?> save(@RequestBody Book book){
         return new ResponseEntity<>(bookService.저장하기(book), HttpStatus.CREATED);   // 201
     }
 
+    @CrossOrigin    // cross-origin 요청 허용   (인텔리제이와 vsCode 포트번호가 달라서)
     @GetMapping("/book/{id}")
     public ResponseEntity<?> detail(@PathVariable Long id){
         return new ResponseEntity<>(bookService.한건가져오기(id), HttpStatus.OK);
     }
 
+    @CrossOrigin    // cross-origin 요청 허용   (인텔리제이와 vsCode 포트번호가 달라서)
     @GetMapping("/book")
     public ResponseEntity<?> findAll(){
         return new ResponseEntity<>(bookService.모두가져오기(), HttpStatus.OK);
     }
 
+    @CrossOrigin    // cross-origin 요청 허용   (인텔리제이와 vsCode 포트번호가 달라서)
     @PutMapping("/book")
     public ResponseEntity<?> update(@RequestBody Book book){
         return new ResponseEntity<>(bookService.수정하기(book), HttpStatus.OK);
     }
 
+    @CrossOrigin    // cross-origin 요청 허용   (인텔리제이와 vsCode 포트번호가 달라서)
     @DeleteMapping("/book/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id){
         return new ResponseEntity<>(bookService.삭제하기(id), HttpStatus.OK);
